4aabac45dbd714bc42e1187c725bb01144f96c99
leonace924 <meobius123@gmail.com>
2022-04-14 04:08:05 -0400

    feat: add craco and tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/index.css";
 
 function App() {
   return (

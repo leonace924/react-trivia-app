22f09c9fbfe75463c015c8043a33c692b4f9e942
leonace924 <meobius123@gmail.com>
2022-04-19 12:59:57 -0400

    chore: clean up and update title

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import "./styles/index.css";
+import React from 'react';
+import { Loading } from 'components/Common';
+import { AppRoutes } from 'routes';
+import 'styles/index.css';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Suspense fallback={<Loading />}>
+      <AppRoutes />
+    </React.Suspense>
   );
 }
 

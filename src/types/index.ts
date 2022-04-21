1e53368c100f72c9344f193079ad42766b67d6cd
leonace924 <meobius123@gmail.com>
2022-04-21 03:18:32 -0400

    feat: add Dto for project

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type QuizDto = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: Array<string>;
+  question: string;
+  type: string;
+};
+
+export type ResultDto = {
+  question: string;
+  isCorrect: boolean;
+};

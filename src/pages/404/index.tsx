bb6e5a790dc803055d6363408770a16254a6ffe0
leonace924 <meobius123@gmail.com>
2022-04-21 04:35:29 -0400

    feat: add 404 page and function

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Paragraph, View } from 'components/Common';
+
+const Page404 = () => {
+  return (
+    <View className="flex items-center justify-center flex-1">
+      <Paragraph className="text-2xl font-semibold text-center">Page not found</Paragraph>
+    </View>
+  );
+};
+
+export default Page404;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,19 @@
+import { Layout } from 'components/Common';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const Home = React.lazy(() => import('pages/Home'));
 const Quiz = React.lazy(() => import('pages/Quiz'));
+const Page404 = React.lazy(() => import('pages/404'));
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/quiz" element={<Quiz />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </Layout>
   );
 };
